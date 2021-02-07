482a9a3f735d111d08286a3a8d6a60243618ad4d
Neves, Guilherme <guilherme.neves@monsanto.com>
2021-02-07 19:41:48 -0300

    adding bash to have regular PATH on cron

--- new-cron-job.sh
+++ new-cron-job.sh
@@ -3,7 +3,7 @@
  sudo crontab -l > newinit
  #echo new cron into cron file
 
- echo "@reboot /root/SelfStreamLive-Jitsi/cron-script.sh" >> newinit #Schedule on reboot
+ echo "@reboot . /etc/profile; /root/SelfStreamLive-Jitsi/cron-script.sh" >> newinit #Schedule on reboot
 
 #install new cron file
  crontab newinit
