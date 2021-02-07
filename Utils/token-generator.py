e52ae013046e4c4f2146bc19995aa576682d0de9
Neves, Guilherme <guilherme.neves@monsanto.com>
2021-02-07 20:21:24 -0300

    added customized token secret based on eventid

--- Utils/token-generator.py
+++ Utils/token-generator.py
@@ -3,20 +3,20 @@ import jwt
 payload = {
   "context": {
     "user": {
-      "avatar": "https:/gravatar.com/avatar/abc123",
-      "name": "John Doe",
-      "email": "jdoe@example.com",
+      "avatar": "https://lh3.googleusercontent.com/proxy/4YqzyyhfsItZW4OJ77LNyxBii0FC1vO8_SM7jXZHo2aOvW2AdX7Ym95ErPbbJvbfs7bRZJuKrsTkc_p4zluZIDGCA9sZeTT66DaHDTgCPhg-4-q8aTq5GLhO8cYnQ00qTQijBQ",
+      "name": "Guilherme Neves",
+      "email": "guilherme.neves@selfstream.live",
       "id": "abcd:a1b2c3-d4e5f6-0abc1-23de-abcdef01fedcba"
     },
     "group": "a123-123-456-789"
   },
   "aud": "jitsi",
   "iss": "selfstreamlive",
-  "sub": "meuevento-hosts.selfstream.live",
+  "sub": "domingo-milagres-host.selfstream.live",
   "room": "*",
   "exp": 1639267200,
   "moderator": True
 }
-secret = "ID-EVENTO-123456789meuevento-hosts"
+secret = "ID-EVENTO-123456789domingo-milagres-host"
 s = jwt.encode(payload, secret, algorithm="HS256")
 print("token: ", s)
--- cron-script.sh
+++ cron-script.sh
@@ -8,6 +8,9 @@ cd /root/SelfStreamLive-Jitsi
 mv /root/SelfStreamLive-Jitsi/host_vars/meuevento-hosts "/root/SelfStreamLive-Jitsi/host_vars/${eventName}"
 
 sed -i '/meet_domain: meuevento-hosts.selfstream.live/c\meet_domain: '"${eventName}"'.selfstream.live' /root/SelfStreamLive-Jitsi/group_vars/meet/main.yml
+sed -i '/vault_token_auth_appsecret: selfstreamlive/c\vault_token_auth_appsecret: '"${secret}"'' /root/SelfStreamLive-Jitsi/group_vars/meet/0secret.yml
+sed -i '/vault_turn_secret: selfstreamlive/c\vault_turn_secret: '"${secret}"'' /root/SelfStreamLive-Jitsi/group_vars/meet/0secret.yml
+
 sed -i 's/\<meuevento-hosts\>/'"${eventName}"'/g' /root/SelfStreamLive-Jitsi/hosts
 sed -i '/jvb.selfstream.live/c\'"${eventName}"'-jvb.selfstream.live' /root/SelfStreamLive-Jitsi/hosts
 sed -i '/jibri.selfstream.live/c\'"${eventName}"'-jibri.selfstream.live' /root/SelfStreamLive-Jitsi/hosts
