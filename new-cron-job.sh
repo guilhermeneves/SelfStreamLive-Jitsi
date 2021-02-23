5181631c9ab3408579463fd50a28ca435adda686
Neves, Guilherme <guilherme.neves@monsanto.com>
2021-02-23 17:39:27 -0300

    added current minute to the auto destroy job

--- new-cron-job.sh
+++ new-cron-job.sh
@@ -4,5 +4,6 @@
  # schedule cron-script to run ansible on reboot
  echo "@reboot . /etc/profile; /root/SelfStreamLive-Jitsi/cron-script.sh" >> newinit
  # schedule cron-script to auto-destroy droplet
- echo "0 */${contracted_hours} * * *  /root/SelfStreamLive-Jitsi/auto-destroy-scheduler.sh" >> newinit
+ current_minute=$(date +"%M")
+ echo "${current_minute} */${contracted_hours} * * *  /root/SelfStreamLive-Jitsi/auto-destroy-scheduler.sh" >> newinit
  crontab newinit
